775e154c67e690e3a6542b93f685bfc037d928f9
enderw12 <enderw2012@gmail.com>
2020-11-02 13:44:28 +0200

    first commit

--- bin/streams.dart
+++ bin/streams.dart
@@ -1,40 +1,69 @@
-import 'dart:convert';
-import 'dart:io';
 import 'dart:async';
-// import 'package:html/dom.dart';
-// import 'package:html/parser.dart';
-import 'package:http/http.dart';
-import 'package:dio/dio.dart';
 
-void main(List<String> arguments) {
-  final String sitemapUrl =
-      arguments.firstWhere((element) => element.endsWith('.xml'));
-  final int threadsArgument = int.parse(arguments
-      .firstWhere((element) => element.startsWith('threads='))
-      .split('=')
-      .last);
+import 'dart:math';
+
+void main() {
+  final bloc = MyBloc();
+
+  // simulating repeated event (eg pressing ++ counter button)
+  Stream<MyEvent> sequenceOfEvents =
+      fc().map((event_number) => MyEvent(event_number));
+
+  //
+
+  sequenceOfEvents.listen((event) {
+    bloc.add(event);
+  });
+
+  bloc.stream.listen((event) {
+    print(event.title);
+  });
 }
 
-initiate(String sitemapUrl) async {
-  final dio = Dio();
-
-  final response = await dio.get(sitemapUrl);
-  final result = response.data;
-  print(result);
-  return result;
-}
-
-List generateWorkers(threadsCount) {
-  List workers = [];
-  for (var i = 0; i < threadsCount - 1; i++) {
-    workers.add(Dio());
+// generator just for demonstartion
+Stream<int> fc() async* {
+  while (true) {
+    final int i = Random().nextInt(1000);
+    await Future.delayed(Duration(milliseconds: i));
+    yield i;
   }
-  return workers;
 }
 
-// // returns stream
-// utilizator(List<Dio> workers, List dataList){
-//   while (dataList.length >0){
-//     workers.firstWhere((dio) => dio.)
-//   }
-// }
+abstract class BlocState {}
+
+abstract class BlocEvent {}
+
+class Bloc<BlocEvent, BlocState> {
+  BlocState mapEventToState(BlocEvent event) {
+    // return BlocState();
+  }
+
+  final _controller = StreamController<BlocEvent>();
+
+  StreamSink<BlocEvent> get event_sink => _controller.sink;
+
+  void add(BlocEvent event) {
+    event_sink.add(event);
+  }
+
+  Stream<BlocState> get stream => _controller.stream
+      .map((event) => mapEventToState(event))
+      .asBroadcastStream();
+}
+
+class MyState extends BlocState {
+  final String title;
+  MyState(this.title);
+}
+
+class MyEvent extends BlocEvent {
+  final int turn;
+  MyEvent(this.turn);
+}
+
+class MyBloc extends Bloc<MyEvent, MyState> {
+  @override
+  MyState mapEventToState(MyEvent event) {
+    return MyState('State #${event.turn}');
+  }
+}
